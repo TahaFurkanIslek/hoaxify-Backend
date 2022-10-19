1490d5418c0bd2cc9e4b6a104dfd41762fe21328
TahaFurkanIslek <105259117+TahaFurkanIslek@users.noreply.github.com>
2022-10-19 17:43:08 +0300

    hoaxify_backend_java

---
+++ ws/src/main/java/com/hoaxify/ws/user/User.java
@@ -0,0 +1,39 @@
+package com.hoaxify.ws.user;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="users")
+public class User {
+	
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	@NotNull(message="{hoaxify.constraints.username.NotNull.message}")
+	@Size(min = 4,max = 255)
+	@UniqueUsername
+	private String username;
+	
+	@NotNull
+	@Size(min = 4,max = 255)
+	private String displayName;
+	
+	@NotNull
+	@Size(min = 8,max = 255)
+	@Pattern(regexp ="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$",message="{hoaxify.constraints.password.Pattern.message}") 
+	private String password;
+
+	
+	
+}
---
+++ ws/src/main/java/com/hoaxify/ws/user/UserService.java
@@ -0,0 +1,28 @@
+package com.hoaxify.ws.user;
+
+//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+	
+	
+	UserRepository userRepository;
+	
+	PasswordEncoder passwordEncoder;
+	
+	public UserService(UserRepository userRepository) {
+		super();
+		this.userRepository = userRepository;
+		this.passwordEncoder=new BCryptPasswordEncoder();
+	}
+
+	
+	
+	public void save(User user) {
+		user.setPassword(this.passwordEncoder.encode(user.getPassword()));
+		userRepository.save(user);
+	}
+}
